3f5571f72369ca071bbdf37964c5886a97dfc772
satyam88 <satyampandey88@gmail.com>
2023-08-15 09:51:15 +0530

    Update backend.tf

--- AWS-VPC/backend.tf
+++ AWS-VPC/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "demo-bucket-00000011"
     key            = "global/s3/terraform.tfstate"
     region         = "ap-south-1"
-    access_key = "AKIAYENA6BLIALZGXZPJ"
-    secret_key = "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"
+    access_key = "AKIAYELZGXZPJ"
+    secret_key = "/c+QGcmI1e6AHLnjDUH"
   }
 }
